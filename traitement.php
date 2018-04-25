f721b678f8642486a86e6e539a3add33db83fd50
Baptiste KIEFFER <baptiste.kief@gmail.com>
2018-04-25 11:11:06 +0200

    vierge

--- traitement.php
+++
@@ -1,8 +0,0 @@
-<?
-if (isset($_POST["validation"]) && $_POST["validation"] = "Envoyer") {
-	$pseudo = $_POST["pseudo"];
-	$password = $_POST["password"];
-	$confirmPassword = $_POST["confirmPassword"];
-	$mail = $_POST["mail"];
-
-	
