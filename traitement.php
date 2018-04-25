d2fa682d5001b217929b3a993da5c701c41091d5
Baptiste KIEFFER <baptiste.kief@gmail.com>
2018-04-25 11:09:21 +0200

    Initial commit

---
+++ traitement.php
@@ -0,0 +1,8 @@
+<?
+if (isset($_POST["validation"]) && $_POST["validation"] = "Envoyer") {
+	$pseudo = $_POST["pseudo"];
+	$password = $_POST["password"];
+	$confirmPassword = $_POST["confirmPassword"];
+	$mail = $_POST["mail"];
+
+	
